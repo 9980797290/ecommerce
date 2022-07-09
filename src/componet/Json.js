9e060536b5f0541f72cf4c6a6e9a38b8ae175990
jhansi <jhansi12mar2000@gmail.com>
2022-07-09 16:47:35 +0530

    Added my project

---
+++ src/componet/Json.js
@@ -0,0 +1,101 @@
+export const porducts = {
+   details: [
+        {
+          name: "SAMSUNG DishWasher (Grey)",
+          productId: "EUR3634767829",
+          brandName: "Samsung",
+          category: "DishWahers",
+          companyAssured: false,
+          rating: "4.2",
+          price: "₹12732",
+          discount: "50%",
+          imageURL: "https://tse4.mm.bing.net/th?id=OIP.9oSmo_D-2gumyKjtiKf7PwD5Es&pid=Api&P=0&w=137&h=164",
+          sellerName: "telform inc.",
+          isNewProduct: true
+        },
+        {
+          name: "Morphy Richards 25 L Convection Microwave Oven  (MWO 25CG, Steel)",
+          productId: "EURO19938202",
+          brandName: "Morphy",
+          category: "Microwave Ovens",
+          companyAssured: true,
+          rating: "3.9",
+          price: "₹10999",
+          discount: "11%",
+          imageURL: "https://i5.walmartimages.com/asr/17c81259-140c-4d37-ba95-56d6b7a7feef.8607280d4b5fb43a855e9444c4c33d46.jpeg",
+          sellerName: "Kwitch Retails",
+          isNewProduct: false
+        },
+        {
+            name: "Refrigirator",
+            productId: "EURO199389203",
+            brandName: "Fitbit",
+            category: "Heir",
+            companyAssured: true,
+          rating: "3.9",
+          price: "₹56992",
+          discount: "",
+          imageURL: "https://tse4.mm.bing.net/th?id=OIP.CFjDZLdEsKI4tPxwsr192AHaMO&pid=Api&P=0&w=104&h=172",
+          sellerName: "RetailNet",
+          isNewProduct: true
+        },
+        {
+            name : "Hot Water Bottle",
+            price :"₹500",
+            brandName:"caster",
+            productId : '1',
+            discription : '',
+            imageURL : 'https://tse2.mm.bing.net/th?id=OIP.I8U1A7yMVVS4H5rKlsrJMQHaHa&pid=Api&P=0&w=163&h=163'
+            
+        },
+        {
+            name : "MacBook pro15",
+            price :"₹1,39,990",
+            productId : '9',
+            discription : '',
+            imageURL : 'https://www.mega.pk/items_images/18115apple_mr932ll_a_15_4_macbook_pro_with_1423733.jpg'
+            
+        },
+        {
+            name : "Hp Inspiron Pentium Silver",
+            price :"₹1,30,990",
+            productId: '7',
+            discription : '',
+            imageURL : 'https://tse4.mm.bing.net/th?id=OIP.-jxE8Qop36ewWX_zbJHItQHaHa&pid=Api&P=0&w=156&h=156'
+            
+        },
+        {
+            name : "Samsung pro12",
+            price :"₹28,890",
+            productId : '7',
+            discription : '',
+            imageURL : 'https://tse2.mm.bing.net/th?id=OIP.V8NVXODUkODhicTKe7MblAHaIL&pid=Api&P=0&w=160&h=177'
+            
+        },
+        {
+            name : "LG Nanocell 139 cm (55 inch) Ultra HD (4K) LED Smart WebOS TV  (55NANO73TPZ)",
+            price :"₹63,990",
+            productId : '4',
+            discription : '',
+            imageURL : 'https://rukminim1.flixcart.com/image/416/416/kqse07k0/television/k/s/6/55nano73tpz-55nano73tpz-lg-original-imag4q3zzt8tfxu8.jpeg?q=70'
+            
+        },
+        {
+            name : "MOTOROLA Revou-Q 139 cm (55 inch) QLED Ultra HD (4K) Smart Android TV with Wireless Gamepad  (55UHDAQMDT5Q)",
+            price :"₹42,990",
+            productId : '5',
+            discription : '',
+            imageURL : 'https://rukminim1.flixcart.com/image/416/416/ku1k4280/television/k/i/u/50uhdaqmdt5q-motorola-original-imag78tyhv4ha3zj.jpeg?q=70'
+            
+        },
+        {
+            name : "Mi Q1 138.8 cm (55 inch) QLED Ultra HD (4K) Smart Android TV",
+            price :"₹56,999",
+            productId : '6',
+            discription : '',
+            imageURL : 'https://rukminim1.flixcart.com/image/416/416/kkoc70w0/television/o/x/j/l55m6-eqg-mi-original-imafzyrjdtba6bqf.jpeg?q=70'
+            
+        },
+   
+      ]
+}
